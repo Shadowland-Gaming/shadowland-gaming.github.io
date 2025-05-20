347c4f639560e3b30438dfc02236a52d593cab3e
Mason Smith <mcsmith3581@live.com>
2025-05-19 20:26:00 -0400

    form re-intro

--- js/script.js
+++ js/script.js
@@ -3,4 +3,4 @@ function calculate() {
     let num2 = parseFloat(document.getElementById("num2").value);
     let result = num1 + num2;
     document.getElementById("result").textContent = "Result: " + result;
-  }
+}
