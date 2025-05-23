12c90c7c47bbba581a5310f7733ae966d90e535d
Mason Smith <mcsmith3581@live.com>
2025-05-22 22:44:27 -0400

    color updates; commenting out bootstrap and checkbox

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
 function calculate() {
-    let num1 = parseFloat(document.getElementById("num1").value);
-    let num2 = parseFloat(document.getElementById("num2").value);
-    let result = num1 + num2;
-    document.getElementById("result").textContent = "Result: " + result;
+    const numShippingCost = 1.19;
+    const numTcgSellerFees = 0.125;
+    const numEbaySellerFees = 0.1325;
+    const numWhatnotPayPalFees = 0.11;
+    //const boolSalesTax = document.getElementById("boolSalesTax").checked;
+
+    let numActualPrice = parseFloat(document.getElementById("numActualPrice").value);
+    let numMarketPrice = parseFloat(document.getElementById("numMarketPrice").value);
+
+    let numSalesTax = numActualPrice * 0.07;
+    let numSalesTaxRounded = Math.round(numSalesTax * 100) / 100;
+
+    let numTotalPrice = numActualPrice + numSalesTaxRounded;
+    let numPercentBelowMarket = ((numMarketPrice - numActualPrice) / numMarketPrice) * 100;
+    let numPercentBelowMarketRounded = Math.round(numPercentBelowMarket * 100) / 100;
+
+    document.getElementById("txtActualPrice").textContent = "Cost: $" + numActualPrice;
+    document.getElementById("txtMarketPrice").textContent = "Market Price: $" + numMarketPrice;
+    document.getElementById("txtSalesTax").textContent = "Sales Tax: $" + numSalesTaxRounded;
 }
