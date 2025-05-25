eac12c2b987971b5cc40aa93fae66f30eca90af3
Mason Smith <mcsmith3581@live.com>
2025-05-24 21:38:55 -0400

    fixing calc

--- js/script.js
+++ js/script.js
@@ -5,28 +5,28 @@ function calculate() {
     const numEbaySellerFee = 0.1325;
     const numWhatnotSellerFee = 0.11;
 
-    let numActualPrice = parseFloat(document.getElementById("numActualPrice").value);
-    let numMarketPrice = parseFloat(document.getElementById("numMarketPrice").value);
-    let numPercentBelowMarket = Math.round(((numMarketPrice - numActualPrice) / numMarketPrice) * 100 * 100) / 100;
+    const numActualPrice = Math.round((parseFloat(document.getElementById("numActualPrice").value)) * 100) / 100;
+    const numMarketPrice = Math.round((parseFloat(document.getElementById("numMarketPrice").value)) * 100) / 100;
+    const numPercentBelowMarket = Math.round((((numMarketPrice - numActualPrice) / numMarketPrice) * 100) * 100) / 100;
 
-    let numSalesTaxRounded = Math.round(numActualPrice * numSalesTax * 100) / 100;
-    let numAcquiredCost = numActualPrice + numSalesTaxRounded;
-
-    let numProfitBeforeSellerFees = numMarketPrice - numAcquiredCost - numShippingCost;
-    let numTcgSellerFeeCalc = Math.round(numMarketPrice * numTcgSellerFee) / 100;
-    let numEbaySellerFeeCalc = Math.round(numMarketPrice * numEbaySellerFee) / 100;
-    let numWhatnotSellerFeeCalc = Math.round(numMarketPrice * numWhatnotSellerFee) / 100;
-
-    let numTcgProfit = Math.round(numProfitBeforeSellerFees - numTcgSellerFeeCalc * 100) / 100;
-    let numEbayProfit = Math.round(numProfitBeforeSellerFees - numEbaySellerFeeCalc * 100) / 100;
-    let numWhatnotProfit = Math.round(numProfitBeforeSellerFees - numWhatnotSellerFeeCalc * 100) / 100;
+    const numSalesTaxCalc = Math.round((numActualPrice * numSalesTax) * 100) / 100;
+    const numAcquiredCost = Math.round((numActualPrice + numSalesTaxCalc) * 100) / 100;
+    const numProfitBeforeSellerFees = Math.round((numMarketPrice - numAcquiredCost - numShippingCost) * 100) / 100;
+    
+    const numTcgSellerFeeCalc = Math.round((numMarketPrice * numTcgSellerFee) * 100) / 100;
+    const numEbaySellerFeeCalc = Math.round((numMarketPrice * numEbaySellerFee) * 100) / 100;
+    const numWhatnotSellerFeeCalc = Math.round((numMarketPrice * numWhatnotSellerFee) * 100) / 100;
+    const numTcgProfit = Math.round((numProfitBeforeSellerFees - numTcgSellerFeeCalc) * 100) / 100;
+    const numEbayProfit = Math.round((numProfitBeforeSellerFees - numEbaySellerFeeCalc) * 100) / 100;
+    const numWhatnotProfit = Math.round((numProfitBeforeSellerFees - numWhatnotSellerFeeCalc) * 100) / 100;
 
     document.getElementById("txtActualPrice").textContent = `Cost: \$${numActualPrice}`;
     document.getElementById("txtMarketPrice").textContent = `Market Price: \$${numMarketPrice}`;
     document.getElementById("txtPercentBelowMarket").textContent = `${numPercentBelowMarket}% Below Market`;
 
-    document.getElementById("txtSalesTax").textContent = `Sales Tax: \$${numSalesTaxRounded}`;
+    document.getElementById("txtSalesTax").textContent = `Sales Tax: \$${numSalesTaxCalc}`;
     document.getElementById("txtAcquiredCost").textContent = `Acquired Cost: \$${numAcquiredCost}`;
+    document.getElementById("txtProfitBeforeSellerFees").textContent = `Profit Before Seller Fees: \$${numProfitBeforeSellerFees}`;
 
     document.getElementById("txtTcgFee").textContent = `\$${numTcgSellerFeeCalc}`;
     document.getElementById("txtTcgProfit").textContent = `\$${numTcgProfit}`;
@@ -37,7 +37,6 @@ function calculate() {
     document.getElementById("txtWhatnotFee").textContent = `\$${numWhatnotSellerFeeCalc}`;
     document.getElementById("txtWhatnotProfit").textContent = `\$${numWhatnotProfit}`;
     document.getElementById("txtWhatnotTotalProfit").textContent = `placeholder`;
-    
-    
+
     
 }
